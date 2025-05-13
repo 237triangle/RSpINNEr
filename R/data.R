ff4b1919c11617820b2c7dbbae59a223647f38c9
Aaron Cohen <cohenaa@indiana.edu>
2025-05-13 12:11:17 -0400

    adding working example dataset

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Example data for spinner regression
+#'
+#' A synthetic dataset with
+#'
+#' @format ## `working_example`
+#' A list with two subobjects:
+#' \describe{
+#'   \item{AA}{A 40 x 40 x 100 array containing 100 matrices for regression}
+#'   \item{y}{A length 100 vector giving the response values}
+#'   ...
+#' }
+#' @source generated, bespoke
+"working_example"
